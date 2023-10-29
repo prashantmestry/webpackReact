fdf31540d3060111b020dc1f7c35d89d6b885111
prashantmestry <mestry.prashant10@gmail.com>
2023-10-29 21:34:15 +0530

    commit other files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "./component/Home";
+
+const App = () =>{
+    return (<div>
+        <h1>Hello React</h1>
+        <Home />
+    </div>)
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    output:{
+        path: path.join(__dirname,"/dist"),
+        filename: "bundle.js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html"
+        })
+    ],
+    devServer:{
+        port: 3030
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                } 
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader","css-loader","sass-loader"]
+            },
+            {
+                test:  /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: "url-loader",
+                options: { limit : false }
+            }
+        ]
+    }
+};
